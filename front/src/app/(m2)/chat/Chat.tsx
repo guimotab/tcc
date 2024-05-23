17ae8eb729efee2407d695d72314296db938f9fd
guimotab <guimota22@gmail.com>
2024-05-23 13:46:17 -0400

    minor

--- front/src/app/(m2)/chat/Chat.tsx
+++ front/src/app/(m2)/chat/Chat.tsx
@@ -25,7 +25,6 @@ const Chat = ({ session }: ChatProps) => {
   const [dataContext, setDataContext] = useState({} as IChatContext)
   const [isAtEndOfChat, setIsAtEndOfChat] = useState(true)
   const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000"
-  console.log("🚀 ~ Chat ~ process.env.NEXT_PUBLIC_BACKEND_URL:", process.env.NEXT_PUBLIC_BACKEND_URL)
   const socket = io(`${BACKEND_URL}/chat`)
 
   useEffect(() => {
